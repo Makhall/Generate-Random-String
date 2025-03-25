93c04131d16f9890c3b6a8e042619c5dd283f97d
akha <siddiqmakhalli17@gmail.com>
2025-03-25 16:57:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+document.getElementById("length").addEventListener("input", function () {
+  document.getElementById("lengthValue").innerText = this.value;
+});
+
+function generateString() {
+  const length = document.getElementById("length").value;
+  const includeUppercase = document.getElementById("uppercase").checked;
+  const includeLowercase = document.getElementById("lowercase").checked;
+  const includeNumbers = document.getElementById("numbers").checked;
+  const includeSymbols = document.getElementById("symbols").checked;
+
+  let charPool = "";
+  if (includeUppercase) charPool += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (includeLowercase) charPool += "abcdefghijklmnopqrstuvwxyz";
+  if (includeNumbers) charPool += "0123456789";
+  if (includeSymbols) charPool += "!@#$%^&*()_+{}[]|:;<>,.?/";
+
+  if (!charPool) {
+    document.getElementById("output").innerText =
+      "Error: No character set selected!";
+    document.getElementById("strength").innerText = "";
+    return;
+  }
+
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += charPool[Math.floor(Math.random() * charPool.length)];
+  }
+
+  document.getElementById("output").innerText = result;
+  document.getElementById("strength").innerText = checkStrength(result);
+}
+
+function checkStrength(password) {
+  let score = 0;
+  if (/[a-z]/.test(password)) score++;
+  if (/[A-Z]/.test(password)) score++;
+  if (/[0-9]/.test(password)) score++;
+  if (/[!@#$%^&*()_+{}\[\]|:;<>,.?/]/.test(password)) score++;
+  if (password.length >= 12) score++;
+
+  const levels = ["Very Weak", "Weak", "Moderate", "Strong", "Very Strong"];
+  return levels[score - 1] || "Very Weak";
+}
+
+// Copy to clipboard
+function copyToClipboard() {
+  const text = document.getElementById("output").innerText;
+  if (!text) {
+    alert("No string to copy!");
+    return;
+  }
+  navigator.clipboard.writeText(text);
+  alert("Copied to clipboard!");
+}
